2ea727b768801aac2dc8fefd3d3505c61dab9ff2
Faraz Arshad <faraz.arshad125@gmail.com>
2025-07-21 04:35:21 +0500

    build hero component

--- edusity/src/Components/Navbar/Navbar.jsx
+++ edusity/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 const Navbar = () => {
   return (
-    <nav className='conatiner'>
-        <img src={logo} alt="" className='logo'/>
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About Us</li>
-            <li>Campus</li>
-            <li>Testimonial</li>
-            <li><button className='btn'>Contact Us</button></li>
-        </ul>
+    <nav className="conatiner">
+      <img src={logo} alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonial</li>
+        <li>
+          <button className="btn">Contact Us</button>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
