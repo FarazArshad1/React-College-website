fd403a4551bdd747ec3368c29d8c39205593e1ca
Faraz Arshad <faraz.arshad125@gmail.com>
2025-07-21 04:19:17 +0500

    create nav bar

--- edusity/src/Components/Navbar/Navbar.jsx
+++ edusity/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Navbar.css'
-
+import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
-    <nav>
-        <img src="" alt="" />
+    <nav className='conatiner'>
+        <img src={logo} alt="" className='logo'/>
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Campus</li>
+            <li>Testimonial</li>
+            <li><button className='btn'>Contact Us</button></li>
+        </ul>
     </nav>
   )
 }
