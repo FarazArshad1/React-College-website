d20fc0661197c54ef057d5c9d8f343adc3b90706
Faraz Arshad <faraz.arshad125@gmail.com>
2025-07-21 06:33:15 +0500

    add transparency feature in navbar & about section

--- edusity/src/Components/Navbar/Navbar.jsx
+++ edusity/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+
+
 const Navbar = () => {
+
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
   return (
-    <nav className="conatiner">
+    <nav className={`conatiner ${sticky? 'dark-nav' :''}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
         <li>Home</li>
