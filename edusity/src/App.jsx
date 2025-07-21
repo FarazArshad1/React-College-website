d098a6e0b59c0d75d322d210c2b6a09fe69d40aa
Faraz Arshad <faraz.arshad125@gmail.com>
2025-07-21 06:49:10 +0500

    add campus section

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
-
+import Campus from "./Components/Campus/Campus";
 const App = () => {
   return (
     <div>
@@ -14,6 +14,8 @@ const App = () => {
         <Title subTitle='OUR PROGRAM' title='What We Offer'/>
         <Programs />
         <About/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
       </div>
     </div>
   );
